918247ae053c95979dabd11a8ad1a24709f36868
Makc <mks59k@gmail.com>
2019-03-03 01:45:10 +0200

    Add project files.

---
+++ Asp.Net_Core_DZ-01_PetitionSite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Asp.Net_Core_DZ_01_PetitionSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
