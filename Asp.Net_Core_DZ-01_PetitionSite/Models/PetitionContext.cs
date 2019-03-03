b2eba4370216f2171c9b8227f09435943c2a892c
Makc <mks59k@gmail.com>
2019-03-03 02:13:28 +0200

    Создание класса петиций и контекста.

---
+++ Asp.Net_Core_DZ-01_PetitionSite/Models/PetitionContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.Net_Core_DZ_01_PetitionSite.Models
+{
+    public class PetitionContext : DbContext
+    {
+        public DbSet<Petition> Petitions { get; set; }
+        // TODO DbSet: list_of_votes, state_of_petitions, users
+    }
+}
