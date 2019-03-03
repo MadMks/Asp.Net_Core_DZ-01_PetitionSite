9e1e14b3a1921307895486575e37d070da905285
Makc <mks59k@gmail.com>
2019-03-03 02:23:57 +0200

    Создание контроллера петиций. Создание представления вывода активных петицый.

---
+++ Asp.Net_Core_DZ-01_PetitionSite/Controllers/PetitionController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.Net_Core_DZ_01_PetitionSite.Controllers
+{
+    public class PetitionController : Controller
+    {
+        // GET: Petition
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Активные
+        public ActionResult Active()
+        {
+            return View();
+        }
+
+        // GET: Все (с архивированными)
+        public ActionResult All()
+        {
+            return View();
+        }
+
+        // GET: Показать одну
+        public ActionResult Show(int id)
+        {
+            ViewBag.PetitionId = id;
+            return View();
+        }
+
+        // GET: Добавить
+        public ActionResult Add()
+        {
+            return View();
+        }
+    }
+}
